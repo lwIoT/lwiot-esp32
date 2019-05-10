45ed9b17f281af0050af1dd2aed9b93026145645
Michel Megens <dev@bietje.net>
2019-05-10 12:02:20 +0200

    tests: remove the EPS32 OLED test
    Remove the OLED display test.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

--- tests/esp32-display_test/main/component.mk
+++
@@ -1,8 +0,0 @@
-#
-# "main" pseudo-component makefile.
-#
-# (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
-
-
-BUILD_DIR = $(PWD)/../../build/debug
-COMPONENT_ADD_LDFLAGS += -L$(BUILD_DIR)/lwiot-core/source -L$(BUILD_DIR)/esp32 -llwiot -llwiot-platform
